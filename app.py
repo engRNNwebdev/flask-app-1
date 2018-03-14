b26481ea9d9653c001c7d1100b1231e052bd9dd9
Jonathan Ferraro <jonathanferraro@rnntvs-mac-mini.rnntv.com>
2018-03-14 10:47:10 -0400

    Alpha Uplynk Control Release

--- app.py
+++ app.py
@@ -2,57 +2,61 @@ from flask import Flask, redirect, url_for, request, render_template
 from flask_bootstrap import Bootstrap
 import uplynk
 #Initialize app and Bootstrap
+
 app = Flask(__name__)
 Bootstrap(app)
 
+#Create web page routes
 @app.route('/')
 def index():
-    return render_template('login.html')
+  return render_template('index.html')
 
-@app.route('/success/<name>')
-def success(name):
-   return 'Successfully started or stopped %s' % name
-
-@app.route('/failure/<name>')
+@app.route('/success/<actionthing>/<name>')
+def success(name,actionthing):
+    if actionthing == 'stop':
+        return 'Successfully stopped on port %s' % name
+    elif actionthing == 'start':
+        return 'Successfully started on port %s' % name
+@app.route('/failure/<actionthing>/<name>')
 def failure(name):
-    return 'The Slicer failed to start or stop %s, please escalate to Engineering' % name
-
+    if actionthing == 'stop':
+        return 'The Slicer failed to stop %s, please escalate to Engineering' % name
+    elif actionthing == 'start':
+        return 'The Slicer failed to start on port %s, please escalate to Engineering' % name
 @app.route('/login', methods = ['POST', 'GET'])
 def login():
-   if request.method == 'POST':
+  if request.method == 'POST':
       user = request.form['nm']
       return redirect(url_for('success',name = user))
-   else:
+  else:
       user = request.args.get('nm')
       return redirect(url_for('success',name = user))
-
 @app.route('/content_start', methods = ['POST', 'GET'])
 def start_slicer():
-   if request.method == 'POST':
+  if request.method == 'POST':
       eid = request.form['eid']
-      uplynk.content_start(eid)
-      return redirect(url_for('success',name = eid))
-   else:
-      user = request.args.get('eid')
-      return redirect(url_for('failure',name = eid))
-
+      slicer = request.form['slicers']
+      uplynk.content_start(slicer,eid)
+      return redirect(url_for('success', actionthing = 'start', name = eid))
+  else:
+      sliced = request.args.get('slicers')
+      return redirect(url_for('failure',name = sliced))
 @app.route('/blackout', methods = ['POST', 'GET'])
 def blackout_slicer():
-   if request.method == 'POST':
-      uplynk.blackout()
-      eid = "dummy id"
-      return redirect(url_for('success',name = eid))
-   else:
-      user = request.args.get('eid')
-      return redirect(url_for('failure',name = eid))
-
+  if request.method == 'POST':
+      slicer = request.form['slicers']
+      uplynk.blackout(slicer)
+      return redirect(url_for('success',actionthing = 'stop', name = slicer))
+  else:
+      sliced = request.args.get('slicers')
+      return redirect(url_for('failure',name = sliced))
 @app.route('/uplynk')
 def uplynk_control():
-    return render_template('uplynk_control.html')
-
+    slicers = uplynk.slicers
+    return render_template('uplynk_control.html',slicers=slicers)
 @app.route('/materialid')
 def material_id():
-    return 'Material ID page goes here'
-
+  return 'Material ID page goes here'
+#Run App and startup
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
--- uplynk.py
+++ uplynk.py
@@ -1,19 +1,27 @@
 import urllib2, json
 
-def content_start(title):
+slicers = [{'id' : 'rnnuplynk1', 'port' : 65009, 'ip': '192.168.101.15'},{'id' : 'rnnuplynk2', 'port' : 65010, 'ip' : '192.1683.101.15'}]
+# slicers = {u'_id': '52a347343be0b32a070e5f4f', u'optid': u'52a347343be0b32a070e5f4e'}
+
+
+def content_start(portnum, title):
     SLICER_IP = "192.168.101.15"  # localhost
-    SLICER_PORT = 65009      # default port
+
+    SLICER_PORT = int(portnum)     # default port
 
     body = json.dumps({"title": title})
     request = urllib2.urlopen("http://%s:%d/content_start" % (SLICER_IP, SLICER_PORT), body)
     response = json.loads(request.read())
     assert response["error"] == 0
 
-def blackout():
+def blackout(portnum):
     SLICER_IP = "192.168.101.15"  # localhost
-    SLICER_PORT = 65009      # default port
+    SLICER_PORT = int(portnum)      # default port
 
     body = json.dumps({"start_timecode":"00:00:00:00"}) # timecode format HH:MM:SS:Frame
     request = urllib2.urlopen("http://%s:%d/blackout" % (SLICER_IP, SLICER_PORT), body)
     response = json.loads(request.read())
+    assert response["error"] == 0
+
+def state(slicer):
     assert response["error"] == 0
