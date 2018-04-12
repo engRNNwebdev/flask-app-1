db4d046cfeba6c0c5bc9c9e9b0b6d42f8b811998
Jonathan <jf0108a@gmail.com>
2018-04-12 16:14:10 -0400

    Isolate NGINX to branch

--- web/create_db.py
+++
@@ -1,8 +0,0 @@
-# create_db.py
-
-from app import db
-from models import *
-db.create_all()
-ed_slicer = Slicer(slicer_id='rnnuplynk1', address='192.168.101.15', port=65009, channel_id='Fios1News Long Island PGM')
-db.session.add(ed_slicer)
-posted = Slicer.query.order_by(Slicer.id.desc()).all()
