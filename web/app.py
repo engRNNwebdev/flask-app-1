f1598ccb8c226f3d26911d1f25b7faf2faf918d0
Jonathan <jf0108a@gmail.com>
2018-04-24 09:54:22 -0400

    adding gunicorn logging

--- web/app.py
+++ web/app.py
@@ -13,6 +13,12 @@ app.config.from_object(BaseConfig)
 db = SQLAlchemy(app)
 from models import *
 
+# gunicorn logging
+if __name__ != '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
+
 #Create web page routes
 
 @app.route('/', methods = ['POST', 'GET'])
